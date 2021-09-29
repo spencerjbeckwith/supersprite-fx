0e145ee94973f21698f4415b52dd28c745196642
spencerjbeckwith <spencerjbeckwith@gmail.com>
2021-09-29 16:42:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# supersprite-fx
+
+This is a repository containing examples of different effects that can be used in supersprite, by replacing the main shader. This is mainly a personal proof-on-concept that supersprite can, effectively, do anything.
---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+export default {
+    input: 'src/main.ts',
+    output: {
+        file: 'build/main.js',
+    },
+    plugins: [
+        typescript({
+            tsconfig: 'tsconfig.json',
+        }),
+        nodeResolve(),
+        terser(),
+    ],
+}
